94d43e873c4f33286f171021fdd9562c9854a45c
Koalski <koalski@gmail.com>
2017-09-15 15:12:20 +0200

    pp

---
+++ Witryny/js/Math.js
@@ -0,0 +1,52 @@
+//console.log('test');
+//Math.sqrt();
+//var x = prompt("podaj wartość: ")
+//console.log(Math.sqrt(x));  // pierw z 25
+var a = 13.2;
+var b = 28.5;
+var minimalna = Math.min(a,b);
+console.log(minimalna);
+var maks = Math.max(a,b);
+console.log(maks);
+var zaokraglenie = Math.round(a);
+console.log(zaokraglenie);
+//abs, round, floor
+/*
+var x = -56;
+var y = -13.2;
+var z = 14.9;
+
+document.write('<br>'+Math.abs(x) + '<br>');
+document.write('<br>'+Math.abs(y) + '<br>');
+document.write('<br>'+Math.abs(z) + '<br>');
+
+document.write('<br>'+Math.round(x) + '<br>');
+document.write('<br>'+Math.round(y) + '<br>');
+document.write('<br>'+Math.round(z) + '<br>');
+
+document.write('<br>'+Math.floor(x) + '<br>');
+document.write('<br>'+Math.floor(y) + '<br>');
+document.write('<br>'+Math.floor(z) + '<br>');
+*/
+
+var p = Math.pow(2,10);
+console.log(p);
+
+var random = Math.random();
+console.log(random);
+//losuj z przedzialu od (0;10>
+
+console.log(Math.floor(Math.random() * 11));
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Witryny/js/funkcje.js
+++ Witryny/js/funkcje.js
@@ -17,4 +17,23 @@ function pole(dl,szer){
     var szer = document.getElementById('b').value;
     return dl*szer;
 }
-    document.write('pole prostokata: ' + pole(a,b) + ' cm2')
+   // document.write('pole prostokata: ' + pole(a,b) + ' cm2')
+
+function poleObjetosc(szer,dl,wys){
+    var pole = szer * dl;
+    var Obj = pole * wys;
+    var wynik = [pole, Obj];
+    return wynik;
+}
+
+console.log(poleObjetosc(2,3,4));
+var pole = poleObjetosc(2,3,10)[0];
+var Obj = poleObjetosc(2,3,10)[1];
+console.log(pole);
+console.log(Obj);
+
+/*var x = prompt('Podaj wartość: ', '0 - pole, 1 - objetosc');
+console.log(poleObjetosc(1,2,3)[x]);*/
+
+console.log(Math.PI);
+//zad dom napisać skrypt który oblicza obwód, pole oraz objętość stożka. Dane użytkownik podaje z klawiatury wykorzystaj obiekt Math uzytkownik wybiera za pomoca pola radio co chce obliczyc, dane wprowadza w formularzu. Wynik wyświetl w bloku
