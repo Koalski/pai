0b7502eb176d43ce61bb56b8e8093e161b427c90
Koalski <31896805+Koalski@users.noreply.github.com>
2017-09-18 10:27:03 +0200

    Add files via upload
    zaddom

---
+++ ind.js
@@ -0,0 +1,13 @@
+function stozek(){
+var h = document.getElementById(h).value; //prompt('podaj wysokość stożka: ');
+var r = document.getElementById(r).value; //prompt('podaj średnice podstawy stożka: ')k;
+
+var Tworzaca = Math.sqrt(Math.pow(h, 2) + Math.pow(r, 2));
+var Pole = Math.PI * Math.pow(r, 2) + Math.PI * r * Tworzaca;
+var Obj = Math.PI / 3 * Math.pow(r, 2) * h;
+
+console.log('Pole to: ' + Math.round(Pole) + " Objętość to: " + Math.round(Obj));
+if(name = Obwód){
+    alert('dupa');
+}
+}
